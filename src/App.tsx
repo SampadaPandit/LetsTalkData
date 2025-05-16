b10a4d04006b2efaef28ea1099bf82b6eb3a0439
SampadaPandit <sampadapandit80819@gmail.com>
2025-05-16 15:26:09 +1000

    Fix Publish Issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import { AuthProvider } from "./contexts/AuthContext";
 
@@ -41,7 +41,7 @@ const App = () => (
       <AuthProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <AnalyticsWrapper>
             <Layout>
               <Routes>
@@ -69,7 +69,7 @@ const App = () => (
               </Routes>
             </Layout>
           </AnalyticsWrapper>
-        </BrowserRouter>
+        </HashRouter>
       </AuthProvider>
     </TooltipProvider>
   </QueryClientProvider>
